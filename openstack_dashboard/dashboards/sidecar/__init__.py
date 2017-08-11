18d28c59541a93ca4d91bc5d11667e787bbc9ec3
Sreenath <sreenath.mm@poornam.com>
2017-08-11 14:43:27 +0530

    Updating the changes

--- openstack_dashboard/dashboards/sidecar/__init__.py
+++
@@ -1,9 +0,0 @@
-# File Name: __init__.py
-#
-# Software: Openstack Horizon [liberity]
-#
-# Package: sidecar dashboard
-# 
-# Start Date: 31th Aug 2016
-#
-# Copyright: 2016@nephoscale
--- openstack_dashboard/dashboards/sidecar/events/__init__.py
+++
@@ -1,9 +0,0 @@
-# File Name: __init__.py
-#
-# Software: Openstack Horizon Liberity 
-#
-# Dash board: Sidecar
-# 
-# Copyright: 2016@nephoscale.com
-#
-# Start Date: 31th Aug 2016
